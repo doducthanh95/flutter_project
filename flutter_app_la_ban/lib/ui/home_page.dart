b73c448627f855a5af05a4bed953860e52e71235
doducthanh95 <doducthanh95@gmail.com>
2020-10-21 16:45:09 +0700

    fix capture screen

--- flutter_app_la_ban/lib/ui/home_page.dart
+++ flutter_app_la_ban/lib/ui/home_page.dart
@@ -11,15 +11,9 @@ import 'package:firebase_dynamic_links/firebase_dynamic_links.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
-import 'package:flutter_google_places/flutter_google_places.dart';
 import 'package:flutter_share/flutter_share.dart';
-import 'package:flutter_styled_toast/flutter_styled_toast.dart';
 import 'package:gallery_saver/gallery_saver.dart';
-import 'package:geolocator/geolocator.dart';
-import 'package:google_maps_flutter/google_maps_flutter.dart';
-import 'package:google_maps_webservice/places.dart';
-import 'package:image_picker/image_picker.dart';
-import 'package:rxdart/subjects.dart';
+import 'package:permission_handler/permission_handler.dart';
 import 'package:screenshot/screenshot.dart';
 import 'dart:ui' as ui;
 
@@ -35,6 +29,7 @@ class _HomePageState extends State<HomePage> with WidgetsBindingObserver {
   DynamicLinkService _dynamicLink;
 
   GlobalKey _containerKey = GlobalKey();
+  GlobalKey _containerKey2 = GlobalKey();
 
   Uint8List _image;
   Uint8List _image2;
@@ -45,14 +40,36 @@ class _HomePageState extends State<HomePage> with WidgetsBindingObserver {
     // TODO: implement initState
     _dynamicLink = DynamicLinkService(_bloc);
     _handleDeepLink();
+    _requestPermission();
 
-    _bloc.streamMapSnapshoot.listen((event) {
-      if (event != null)
-        setState(() {
-          _image = event;
-        });
+    _bloc.streamMapSnapshoot.listen((event) async {
+      if (event != null) {
+        _image = event;
+        _capturePng();
+      }
     });
     super.initState();
+  }
+
+  void _capturePng() async {
+    RenderRepaintBoundary renderRepaintBoundary =
+        _containerKey.currentContext.findRenderObject();
+    ui.Image boxImage = await renderRepaintBoundary.toImage(pixelRatio: 1);
+    ByteData byteData =
+        await boxImage.toByteData(format: ui.ImageByteFormat.png);
+    Uint8List uInt8List = byteData.buffer.asUint8List();
+    _image2 = uInt8List;
+    _isShowChildScreen = true;
+    setState(() {
+      Future.delayed(Duration(seconds: 1)).then((value) async {
+        var _newFile = await _screenshotController.capture(pixelRatio: 20);
+        await GallerySaver.saveImage(_newFile.path).then((value) {
+          setState(() {
+            _isShowChildScreen = false;
+          });
+        });
+      });
+    });
   }
 
   void _handleDeepLink() async {
@@ -174,24 +191,13 @@ class _HomePageState extends State<HomePage> with WidgetsBindingObserver {
     Navigator.pop(context);
     print("File Saved to Gallery");
     _bloc.takeMapSnapshot();
+  }
 
-    RenderRepaintBoundary renderRepaintBoundary =
-        _containerKey.currentContext.findRenderObject();
-    ui.Image boxImage = await renderRepaintBoundary.toImage(pixelRatio: 1);
-    ByteData byteData =
-        await boxImage.toByteData(format: ui.ImageByteFormat.png);
-    Uint8List uInt8List = byteData.buffer.asUint8List();
-    _image2 = uInt8List;
-    setState(() {
-      _isShowChildScreen = true;
-    });
+  _requestPermission() async {
+    Map<Permission, PermissionStatus> statuses = await [
+      Permission.storage,
+    ].request();
 
-    var _newFile = await _screenshotController.capture(pixelRatio: 10);
-    await GallerySaver.saveImage(_newFile.path);
-    Future.delayed(Duration(seconds: 2)).then((value) {
-      setState(() {
-        _isShowChildScreen = false;
-      });
-    });
+    final info = statuses[Permission.storage].toString();
   }
 }
